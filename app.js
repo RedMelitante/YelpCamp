7967c4d710af31c8450705a8c8965e8f9df5129f
Rey Adrian Melitante <Redmelitante@gmail.com>
2022-05-09 14:32:35 +0800

    415. New and Create
    done

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({extended: true}));
+
 app.get('/', (req, res) => {
     res.render('home')
 });
@@ -25,12 +27,24 @@ app.get('/', (req, res) => {
 app.get('/campgrounds', async (req, res) => {
    const campgrounds = await Campground.find({});
    res.render('campgrounds/index', { campgrounds })
+});
+
+app.get('/campgrounds/new', (req, res) =>{
+    res.render('campgrounds/new');
+})
+
+app.post('/campgrounds', async (req, res) =>{
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground.id}`)
 })
 
 app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', {campground});
-})
+});
+
+
 
 app.listen(3000, () => {
     console.log('Serving on Port 3000');
