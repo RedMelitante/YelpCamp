f75753e0573d4fbd79c19b1ff2c7b185dab78121
Rey Adrian Melitante <Redmelitante@gmail.com>
2022-05-14 16:38:10 +0800

    445. Basic Error Handler
    Done

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
+const res = require('express/lib/response');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp'),
 
@@ -36,10 +37,14 @@ app.get('/campgrounds/new', (req, res) =>{
     res.render('campgrounds/new');
 })
 
-app.post('/campgrounds', async (req, res) =>{
+app.post('/campgrounds', async (req, res, next) =>{
+    try {
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
+    } catch (e) {
+        next(e);
+    }
 })
 
 app.get('/campgrounds/:id', async (req, res) => {
@@ -55,14 +60,18 @@ app.get('/campgrounds/:id/edit', async (req, res) => {
 app.put('/campgrounds/:id', async (req, res) => {
     const {id} = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-    res.redirect(`/campgrounds/${campground._id}`);
+    res.redirect('/campgrounds/${campground._id}');
 });
 
 app.delete('/campgrounds/:id', async (req, res) => {
     const {id} = req.params;
     await Campground.findByIdAndDelete(id);
-    res.redirect(`/campgrounds`);
+    res.redirect('/campgrounds');
 });
+
+app.use((err, req, res, next) => {
+    res.send('ohh boy, something went wrong')
+})
 
 app.listen(3000, () => {
     console.log('Serving on Port 3000');
