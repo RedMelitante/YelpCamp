47a3c4f6f14840b98aa62d9c11b9bcc2a0e751c7
Rey Adrian Melitante <Redmelitante@gmail.com>
2022-05-17 21:16:09 +0800

    511. Configuring Passport
    Done

--- app.js
+++ app.js
@@ -7,12 +7,18 @@ const flash = require('connect-flash');
 const { campgroundSchema, reviewSchema } = require('./schemas.js')
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
+
 const Campground = require('./models/campground');
 const res = require('express/lib/response');
 const Review = require('./models/review');
 
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
+
+
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp'),
 
@@ -46,6 +52,13 @@ const sessionConfig = {
 }
     app.use(session(sessionConfig));
     app.use(flash());
+
+    app.use(passport.initialize());
+    app.use(passport.session());
+    passport.use(new LocalStrategy(User.authenticate()));
+
+    passport.serializeUser(User.serializeUser());
+    passport.deserializeUser(User.deserializeUser());
     
     app.use((req, res, next) => {
         res.locals.success = req.flash('success');
