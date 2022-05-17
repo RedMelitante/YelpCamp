fd2813b97c98b764ae3a183b7c074bcd710037b9
Rey Adrian Melitante <Redmelitante@gmail.com>
2022-05-17 16:43:06 +0800

    495. Flash Errors Partial
    Done

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -37,11 +37,19 @@ const validateCampground = (req, res, next) => {
     
     router.get('/:id', catchAsync( async (req, res) => {
         const campground = await Campground.findById(req.params.id).populate('reviews');
+        if (!campground){
+            req.flash('error', 'Cannot find that Campground');
+            return res.redirect('/campgrounds');
+        }
         res.render('campgrounds/show', {campground});
     }));
     
     router.get('/:id/edit', catchAsync( async (req, res) => {
         const campground = await Campground.findById(req.params.id);
+        if (!campground){
+            req.flash('error', 'Cannot find that Campground');
+            return res.redirect('/campgrounds');
+        }
         res.render('campgrounds/edit', {campground});
     }));
     
