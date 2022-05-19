6dce9dc78b898315ab59dbabcaee7c4f98fca740
Rey Adrian Melitante <Redmelitante@gmail.com>
2022-05-19 17:46:31 +0800

    525. Reviews Authorization
    Done

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -25,7 +25,13 @@ const Campground = require('../models/campground');
     }))
     
     router.get('/:id', catchAsync( async (req, res) => {
-        const campground = await (await Campground.findById(req.params.id).populate('reviews')).populate('author');
+        const campground = await Campground.findById(req.params.id).populate({
+            path:'reviews',
+            populate: {
+                path:'author'
+            }
+        }).populate('author');
+        console.log(campground);
         if (!campground){
             req.flash('error', 'Cannot find that Campground');
             return res.redirect('/campgrounds');
