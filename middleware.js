6240dbb3c008a307c2ac819389d90deb3d188d22
Rey Adrian Melitante <Redmelitante@gmail.com>
2022-05-19 11:15:09 +0800

    519. Return to behavior
    done

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 module.exports.isLoggedIn = (req, res, next) => {
+    req.session.returnTo = req.originalUrl
         if(!req.isAuthenticated()){
             req.flash('error','you must be singed in first');
             return res.redirect('/login');
--- routes/users.js
+++ routes/users.js
@@ -9,14 +9,16 @@ router.get('/register', (req, res) => {
     res.render('users/register');
 });
 
-router.post('/register', catchAsync (async(req, res) => {
+router.post('/register', catchAsync (async(req, res, next) => {
     try {
     const {email, username, password} = req.body;
     const user = new User({email, username});
     const registeredUser = await User.register(user, password);
-    console.log(registeredUser);
-    req.flash('success', 'Welcome to YelpCamp');
-    res.redirect('/campgrounds');
+    req.logIn(registeredUser, err => {
+        if (err) return next(err);
+        req.flash('success', 'Welcome to YelpCamp');
+        res.redirect('/campgrounds');
+    })
     } catch (e) {
         req.flash('error', e.message);
         res.redirect('register');
@@ -29,7 +31,9 @@ router.get('/login', (req, res) => {
 
 router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), (req, res) => {
     req.flash('success', 'welcome back');
-    res.redirect('/campgrounds');
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
 })
 
 router.get('/logout', (req, res) => {
