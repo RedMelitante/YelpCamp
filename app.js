f1f4b00de4659e4810c0ca2f890c0756758f009a
Rey Adrian Melitante <Redmelitante@gmail.com>
2022-05-29 22:37:15 +0800

    ready to try deploying

--- app.js
+++ app.js
@@ -23,9 +23,11 @@ const mongoSanitize = require('express-mongo-sanitize');
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
+const MongoStore = require('connect-mongo'); // check docs
+// const dbUrl = process.env.DB_URL;
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp'),
-
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+mongoose.connect('dbUrl');
 //const db = mongoose.connection;
 mongoose.connection.on("error", console.error.bind(console, "connection error:"));
 mongoose.connection.once("open", () => {
@@ -47,75 +49,104 @@ app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(mongoSanitize({
-    replaceWith:'_'
-}))
+    replaceWith:'_',
+}),
+);
+
+const secret = process.env.SECRET || 'thisshouldbeabettersecret';
+
+const store = new MongoStore({
+    url: dbUrl,
+    secret,
+    touchAfter: 24 * 60 * 60 
+});
+
+store.on('error', function (err) {
+    console.log('session error', err)
+});
 
 
 
-const sessionConfig = {
-    name: 'session',
-    secret: 'thisshouldbeabettersecret',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        httpOnly: true,
-        // secure: true,
-        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24 * 7,
+// const sessionConfig = {
+//     store,
+//     name: 'session',
+//     secret,
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: {
+//         httpOnly: true,
+//         // secure: true,
+//         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+//         maxAge: 1000 * 60 * 60 * 24 * 7,
+//     }
+// }
+
+    app.use(session({
+        store: MongoStore.create(options),
+        name: 'session',
+        secret,
+        resave: false,
+        saveUninitialized: true,
+        cookie: {
+            httpOnly: true,
+            // secure: true,
+            expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+            maxAge: 1000 * 60 * 60 * 24 * 7,
+        }
     }
-}
-
-    app.use(session(sessionConfig));
+    ));
     app.use(flash());
-    app.use(helmet());
+    app.use(helmet({
+        contentSecurityPolicy: false,
+      }));
 
-    const scriptSrcUrls = [
-        "https://stackpath.bootstrapcdn.com",
-        "https://getbootstrap.com",
-        "https://api.tiles.mapbox.com",
-        "https://api.mapbox.com",
-        "https://kit.fontawesome.com",
-        "https://cdnjs.cloudflare.com",
-        "https://cdn.jsdelivr.net",
-    ];
-    const styleSrcUrls = [
-        "https://kit-free.fontawesome.com",
-        "https://stackpath.bootstrapcdn.com",
-        "https://api.mapbox.com",
-        "https://api.tiles.mapbox.com",
-        "https://fonts.googleapis.com",
-        "https://use.fontawesome.com",
-        "https://cdn.jsdelivr.net"
-    ];
-    const connectSrcUrls = [
-        "https://api.mapbox.com",
-        "https://a.tiles.mapbox.com",
-        "https://b.tiles.mapbox.com",
-        "https://events.mapbox.com",
-    ];
-    const fontSrcUrls = [];
-    app.use(
-        helmet.contentSecurityPolicy({
-            directives: {
-                defaultSrc: [],
-                connectSrc: ["'self'", ...connectSrcUrls],
-                scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
-                styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
-                workerSrc: ["'self'", "blob:"],
-                childSrc: ["blob:"],
-                objectSrc: [],
-                imgSrc: [
-                    "'self'",
-                    "blob:",
-                    "data:",
-                    "https://res.cloudinary.com/dwkmibftv", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
-                    "https://images.unsplash.com",
-                    "https://res.cloudinary.com",
-                ],
-                fontSrc: ["'self'", ...fontSrcUrls],
-            },
-        })
-    );
+    // const scriptSrcUrls = [
+    //     "https://stackpath.bootstrapcdn.com",
+    //     "https://getbootstrap.com",
+    //     "https://api.tiles.mapbox.com",
+    //     "https://api.mapbox.com",
+    //     "https://kit.fontawesome.com",
+    //     "https://cdnjs.cloudflare.com",
+    //     "https://cdn.jsdelivr.net",
+    // ];
+    // const styleSrcUrls = [
+    //     "https://kit-free.fontawesome.com",
+    //     "https://stackpath.bootstrapcdn.com",
+    //     "https://api.mapbox.com",
+    //     "https://api.tiles.mapbox.com",
+    //     "https://fonts.googleapis.com",
+    //     "https://use.fontawesome.com",
+    //     "https://cdn.jsdelivr.net"
+    // ];
+    // const connectSrcUrls = [
+    //     "https://api.mapbox.com",
+    //     "https://a.tiles.mapbox.com",
+    //     "https://b.tiles.mapbox.com",
+    //     "https://events.mapbox.com",
+    // ];
+    // const fontSrcUrls = [];
+    // app.use(
+    //     helmet.contentSecurityPolicy({
+    //         directives: {
+    //             defaultSrc: [],
+    //             connectSrc: ["'self'", ...connectSrcUrls],
+    //             scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+    //             styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+    //             workerSrc: ["'self'", "blob:"],
+    //             childSrc: ["blob:"],
+    //             objectSrc: [],
+    //             imgSrc: [
+    //                 "'self'",
+    //                 "blob:",
+    //                 "data:",
+    //                 "https://res.cloudinary.com/dwkmibftv", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+    //                 "https://images.unsplash.com",
+    //                 "https://res.cloudinary.com",
+    //             ],
+    //             fontSrc: ["'self'", ...fontSrcUrls],
+    //         },
+    //     })
+    // );
 
     app.use(passport.initialize());
     app.use(passport.session());
